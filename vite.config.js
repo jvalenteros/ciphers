ce71b2ef12b150a9f1742fd18eb7c8d3988cf49b
jvalenteros <52338531+jvalenteros@users.noreply.github.com>
2025-03-26 01:28:34 -0500

    vite.config.js base change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ciphers-3/', // GitHub Pages repository name
+  base: '/ciphers/', // GitHub Pages repository name
 })
